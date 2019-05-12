1d94f093640f32ff20d1ac9996601161c053e0bf
Olga <baushonok@gmail.com>
2019-05-12 21:59:16 +0300

    Add comments counter

--- src/comments/index.tsx
+++ src/comments/index.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { ChangeEvent, Component } from 'react';
 
 import faker from 'faker';
 
 import Comment from 'components/Comment';
 
-export default function Comments() {
-  function generateComments(amount: number = 1) {
+interface IState {
+  amountOfComments: number;
+}
+
+export default class Comments extends Component<{}, IState> {
+  public state = {
+    amountOfComments: 1,
+  };
+  public render() {
+    return (
+      <>
+        <h1 className="ui header">Comments</h1>
+        <label htmlFor="counter" className="ui label">
+          Input amount of components:
+        </label>
+        <input
+          type="number"
+          id="counter"
+          min="0"
+          value={this.state.amountOfComments}
+          onChange={this.handleChangeAmountOfComments}
+          className="ui input"
+        />
+        <ul className="ui container comments">{this.generateComments()}</ul>
+      </>
+    );
+  }
+  private handleChangeAmountOfComments = (event: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ amountOfComments: parseInt(event.currentTarget.value, 10) });
+  };
+  private generateComments = () => {
+    const { amountOfComments: amount } = this.state;
     const rows = [];
     for (let i = 0; i < amount; i++) {
       rows.push(
@@ -21,12 +51,5 @@ export default function Comments() {
     }
 
     return rows;
-  }
-
-  return (
-    <>
-      <h1 className="ui header">Comments</h1>
-      <ul className="ui container comments">{generateComments(5)}</ul>
-    </>
-  );
+  };
 }
