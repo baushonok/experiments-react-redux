1c748db056dd2028b22d7e8a83b24aed9283a270
Olga <baushonok@gmail.com>
2019-06-16 16:58:51 +0300

    Put images list render into a separated method

--- src/components/ImagesList/index.tsx
+++ src/components/ImagesList/index.tsx
@@ -6,15 +6,14 @@ import './index.css';
 
 export default class ImagesList extends Component<IProps> {
   public render() {
-    const { images } = this.props;
-    return (
-      <ul className="images-list">
-        {images.map(current => (
-          <li key={current.id} className="images-list__item">
-            <img src={current.urls.small} alt={current.description} />
-          </li>
-        ))}
-      </ul>
-    );
+    return <ul className="images-list">{this.renderImagesList()}</ul>;
   }
+  private renderImagesList = () => {
+    const { images } = this.props;
+    return images.map(current => (
+      <li key={current.id} className="images-list__item">
+        <img src={current.urls.small} alt={current.description} />
+      </li>
+    ));
+  };
 }
